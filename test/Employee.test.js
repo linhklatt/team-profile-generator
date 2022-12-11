fb3b49ebabdd85b4d7e28c1e9b48e6662a8ce9ec
Linh Klatt <you@example.com>
2022-12-11 14:15:10 -0800

    wrote Employee.test.js fuction

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,3 +1,5 @@
+const { it } = require("node:test");
+
 const Employee = requirer("./lib/Employee");
 
 describe("Testing Employee Class", () => {
@@ -13,5 +15,21 @@ describe("Testing Employee Class", () => {
     expect(employee.name).toBe(name);
     expect(employee.id).toBe(id);
     expect(employee.email).toBe(email);
+  });
+  it("should be able to get name via constructor argument", () => {
+    const name = Linh;
+    const employee = new Employee(name, 22, "linhklatt@yahoo.com");
+    expect(employee.getName()).toBe(employee.name);
+  });
+
+  it("should be able to get id via constructor argument", () => {
+    const id = 22;
+    const employee = new Employee("Linh", id, "linhklatt@yahoo.com");
+    expect(employee.getId()).toBe(employee.id);
+  });
+  it("should be able to set email via constructor argument", () => {
+    const email = "linhklatt@yahoo.com";
+    const employee = new Employee("Linh", 22, email);
+    expect(employee.getEmail()).toBe(employee.email);
   });
 });
